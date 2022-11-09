4213eb4002ff9b7c489f0f469adc7e7c47f69869
Bruce Merry <bmerry@gmail.com>
2022-11-09 22:05:07 +0200

    Add a bunch more fields

--- src/fields.rs
+++ src/fields.rs
@@ -54,30 +54,38 @@ impl<'a> Field<'a> {
         }
     }
 
-    pub const fn voltage(offset: usize, group: &'a str, id: &'a str) -> Self {
+    pub const fn voltage_name(offset: usize, group: &'a str, name: &'a str, id: &'a str, scale: f64) -> Self {
         Field {
             field_type: FieldType::Voltage,
             offset,
             group,
-            name: "Voltage",
+            name,
             id,
-            scale: 0.1,
+            scale,
             bias: 0.0,
             unit: "V"
         }
     }
 
-    pub const fn current(offset: usize, group: &'a str, id: &'a str) -> Self {
+    pub const fn voltage(offset: usize, group: &'a str, id: &'a str, scale: f64) -> Self {
+        Field::voltage_name(offset, group, "Voltage", id, scale)
+    }
+
+    pub const fn current_name(offset: usize, group: &'a str, name: &'a str, id: &'a str, scale: f64) -> Self {
         Field {
             field_type: FieldType::Current,
             offset,
             group,
-            name: "Current",
+            name,
             id,
-            scale: 0.01,
+            scale,
             bias: 0.0,
             unit: "A"
         }
+    }
+
+    pub const fn current(offset: usize, group: &'a str, id: &'a str, scale: f64) -> Self {
+        Field::current_name(offset, group, "Current", id, scale)
     }
 
     pub const fn temperature_name(
@@ -166,20 +174,34 @@ pub const FIELDS: &[Field] = &[
     Field::energy(74, "Battery", "Total discharge", "battery_discharge_total"),
     Field::energy(82, "Grid", "Total import", "grid_import_total"),
     Field::frequency(84, "Grid", "grid_frequency"),
-    Field::energy(88, "Grid", "Total export", "grid_export_total"),
+    Field::energy(88, "Grid", "Total export", "grid_export_total"), // Might also be 112
     Field::energy(96, "Load", "Total consumption", "load_consumption_total"),
     Field::temperature_name(106, "Inverter", "DC Temperature", "inverter_temperature_dc"),
     Field::temperature_name(108, "Inverter", "AC Temperature", "inverter_temperature_ac"),
     Field::energy(118, "PV", "Total production", "pv_production_total"),
     Field::charge(140, "Battery", "Capacity", "battery_capacity"),
-    Field::voltage(176, "Grid", "grid_voltage"),
-    Field::voltage(184, "Load", "load_voltage"),
-    Field::power(216, "Grid", "grid_power"),
-    Field::power(228, "Load", "load_power"),
+    Field::voltage_name(144, "PV", "Voltage 1", "pv_voltage 1", 0.1),
+    Field::current_name(146, "PV", "Current 1", "pv_current_1", 0.1),
+    Field::voltage_name(148, "PV", "Voltage 2", "pv_voltage_2", 0.1),
+    Field::current_name(150, "PV", "Current 2", "pv_current_2", 0.1),
+    Field::voltage(176, "Grid", "grid_voltage", 0.1), // Might also be 180
+    Field::voltage(184, "Load", "load_voltage", 0.1), // Might also be 188
+    Field::current(196, "Grid", "grid_current", 0.01),
+    Field::current(204, "Load", "load_current", 0.01),
+    Field::power(216, "Grid", "grid_power"), // Might also be 214 or 220
+    Field::power(222, "Inverter", "inverter_power"), // Might also be 226
+    Field::power(228, "Load", "load_power"), // Or 232 (one is P-load, the other P-Load-L1)
     Field::temperature(240, "Battery", "battery_temperature"),
+    Field::voltage(242, "Battery", "battery_voltage", 0.01),
     Field::state_of_charge(244, "Battery", "battery_soc"),
     Field::power(248, "PV", "pv_power"),
     Field::power(256, "Battery", "battery_power"),
-    Field::current(258, "Battery", "battery_current"),
-    Field::frequency(260, "Load", "load_frequency"),
+    Field::current(258, "Battery", "battery_current", 0.01),
+    Field::frequency(260, "Load", "load_frequency"), // Might also be 262
+    Field::voltage_name(276, "BMS", "Charge Voltage", "bms_charge_voltage", 0.01),
+    Field::current_name(280, "BMS", "Charge Limit Current", "bms_charge_limit_current", 1.0),
+    Field::current_name(282, "BMS", "Discharge Limit Current", "bms_discharge_limit_current", 1.0),
+    Field::voltage(286, "BMS", "bms_voltage", 0.01),
+    Field::current(288, "BMS", "bms_current", 1.0),
+    Field::temperature(290, "BMS", "bms_temperature"),
 ];
